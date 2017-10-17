61d85517eee9fc6c4c642255096b3d8ae34ca17a
bouiboui <phpman1@gmail.com>
2017-10-17 22:30:11 +0200

    Added CommonJS export

--- index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-export {default as SocketDispatcher} from './src/SocketDispatcher'
-export {default as SocketProvider} from './src/SocketProvider'
-export {default as SocketListener} from './src/SocketListener'
-export {default as withSocket} from './src/withSocket'
---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export {default as SocketDispatcher} from './SocketDispatcher'
+export {default as SocketProvider} from './SocketProvider'
+export {default as SocketListener} from './SocketListener'
+export {default as withSocket} from './withSocket'
